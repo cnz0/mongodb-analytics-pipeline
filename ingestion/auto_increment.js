cd3bdd060b453a15f69aea1e9d94dfeb12e1834b
cnz0 <cnz0@proton.me>
2026-02-27 14:19:39 +0100

    Formating data #1

--- ingestion/auto_increment.js
+++ ingestion/auto_increment.js
@@ -1,12 +1,10 @@
-db.counters.insertOne({ _id: "video_seq", value: 0 });
-
 function getNextSequence(name) {
   const ret = db.counters.findOneAndUpdate(
     { _id: name },
     { $inc: { value: 1 } },
     {
       upsert: true,
-      returnDocument: "after"   // zwraca już zaktualizowany dokument
+      returnDocument: "after"
     }
   );
   return ret.value;
